4c698950ec20cad72d89647735b7b73d3da0d3ec
walterlee <walter.lee9@gmail.com>
2019-06-10 18:14:03 -0500

    converted .js to .jsx

---
+++ src/components/Intro.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Intro extends React.Component {
+
+    render() {
+        return (
+            <div className="intro-container">
+                <div className="intro">
+                    This is where introduction will appear.
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Intro;
