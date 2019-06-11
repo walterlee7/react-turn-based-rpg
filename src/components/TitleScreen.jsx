bb658c393be3554e2a3e4d72f29663651f8ff0f6
walterlee <walter.lee9@gmail.com>
2019-06-10 19:25:13 -0500

    TitleScreen update to avoid render warning

--- src/components/TitleScreen.jsx
+++ src/components/TitleScreen.jsx
@@ -3,20 +3,32 @@ import ReactDOM from 'react-dom';
 import Intro from './Intro';
 
 class TitleScreen extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            display: 1
+        }
+    }
 
     startGame() {
-        ReactDOM.render(<Intro />, document.getElementById('title-screen'));
+        ReactDOM.render(<Intro />, document.getElementById('test'));
+
+        this.setState({
+            display: 'none'
+        })
     }
 
     render() {
         return (
             <div id="title-screen" >
-                <div className="title">React Turn Based RPG</div>
-                <div className="start-game">
+                <div style={{ display: this.state.display }} className="title">React Turn Based RPG</div>
+                <div style={{ display: this.state.display }} id="start-game">
                     <button onClick={() => this.startGame()} className="start-button">
                         Start Game
                     </button>
                 </div>
+                <div id='test' />
             </div>
         );
     }
