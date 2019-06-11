4a57ed8d2330f60ee1425ea38d4b2bf54b55d3be
walterlee <walter.lee9@gmail.com>
2019-06-10 19:41:27 -0500

    Updates

---
+++ precache-manifest.753db4effc04a031f727ac6b0de1cab2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26fd5efb2ab381a0b2ca43217e0790b2",
+    "url": "/react-turn-based-rpg/index.html"
+  },
+  {
+    "revision": "9f8da2a407012883ca18",
+    "url": "/react-turn-based-rpg/static/css/main.d31fe9e2.chunk.css"
+  },
+  {
+    "revision": "4385d4317398679ad6bc",
+    "url": "/react-turn-based-rpg/static/js/2.2df21c95.chunk.js"
+  },
+  {
+    "revision": "9f8da2a407012883ca18",
+    "url": "/react-turn-based-rpg/static/js/main.3b274b7f.chunk.js"
+  },
+  {
+    "revision": "402ea09176ef310217c2",
+    "url": "/react-turn-based-rpg/static/js/runtime~main.9883d49c.js"
+  }
+]);
