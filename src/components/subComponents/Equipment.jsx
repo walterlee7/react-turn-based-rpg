6b050f582566474490260106cb20241d70c2faab
walterlee <walter.lee9@gmail.com>
2019-06-19 13:21:46 -0500

    equipment update

--- src/components/subComponents/Equipment.jsx
+++ src/components/subComponents/Equipment.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import '../css/Equipment.css';
+import { playerPortraits } from '../assets/playerPortraits/playerPortraits';
 
 export default class Equipment extends React.Component {
     render() {
         return (
-            <div className="equipment">
-                <div className="equipment-container">
-                    No Equipment Data
+            <React.Fragment>
+                <div className="player-equipment">
+                    <div className="player-equipment-container">
+                        <img id="playerEquipmentImage" src={playerPortraits[0].url} alt="player" ></img>
+                    </div>
                 </div>
-            </div>
+                <div className="equipment">
+                    <div className="equipment-container">
+                        No Equipment Data
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
