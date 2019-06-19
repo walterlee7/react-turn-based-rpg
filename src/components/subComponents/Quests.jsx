746ec129e8114dff5cdebf9873790e83e67b4728
walterlee <walter.lee9@gmail.com>
2019-06-18 19:59:03 -0500

    quest update

--- src/components/subComponents/Quests.jsx
+++ src/components/subComponents/Quests.jsx
@@ -2,16 +2,60 @@ import React from 'react';
 import '../css/Quests.css';
 
 export default class Quests extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            mainDisplay: 'none',
+            sideDisplay: 'none',
+        }
+    }
+
+
+    mainQuestDisplay() {
+        this.setState({
+            mainDisplay: 'inline-block',
+            sideDisplay: 'none'
+        })
+    }
+
+    sideQuestDisplay() {
+        this.setState({
+            sideDisplay: 'inline-block',
+            mainDisplay: 'none'
+        })
+    }
+
     render() {
         return (
-            <div className="quest">
-                <button className="questButton">
-                    Main Quest
-                </button>
-                <button className="questButton">
-                    Side Quest
-                </button>
-            </div>
+            <React.Fragment>
+                <div className="quest">
+
+                    <button onClick={() => this.mainQuestDisplay()} className="questButton">
+                        Main Quest
+                    </button>
+                    <button onClick={() => this.sideQuestDisplay()} className="questButton">
+                        Side Quest
+                    </button>
+
+                </div>
+                <div className="quest-text">
+                    <div style={{ display: this.state.mainDisplay }} className="main-quest-container">
+
+                        Investigate the Haunted House
+
+                        <hr />
+                        <ol style={{ float: 'left' }}>
+                            - Enter the House...
+                        </ol>
+                    </div>
+                    <div style={{ display: this.state.sideDisplay }} className="side-quest-container">
+                        No Side Quest Data.
+                    </div>
+                </div>
+            </React.Fragment>
+
+
         )
     }
 }
