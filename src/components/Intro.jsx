7ed7190b660d48d8bdef20f1180a031543b5aa65
walterlee <walter.lee9@gmail.com>
2019-06-25 19:32:38 -0500

    intro css and content update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './css/Intro.css';
 import Game from './Game';
 
 class Intro extends React.Component {
@@ -23,30 +24,48 @@ class Intro extends React.Component {
             <React.Fragment>
                 <div style={{ display: this.state.displayIntro }} className="intro-container">
                     <div className="intro">
-                        <div>
-                            This is where the introduction will appear.
-                    </div>
-                        <div>
-                            ...
-                    </div>
-                        <div>
-                            ...
-                    </div>
-                        <div>
-                            ...
-                    </div>
-                        <div>
-                            ...
-                    </div>
-                        <div>
-                            ...
-                    </div>
-                        <div>
-                            ...
-                    </div>
-                        <div>
-                            ...
-                    </div>
+                        <div className='intro-one-container'>
+                            <div className='intro-one'>
+                                ?????:
+                            </div>
+                            <div className='intro-one'>
+                                Hello........
+                            </div>
+                        </div>
+
+                        <br />
+
+                        <div className='intro-two-container'>
+                            <div className='intro-two'>
+                                :????
+                            </div>
+                            <div className='intro-two' >
+                                Hello........
+                            </div>
+                        </div>
+
+                        <br />
+
+                        <div className='intro-one-container'>
+                            <div className='intro-one'>
+                                ?????:
+                            </div>
+                            <div className='intro-one'>
+                                World.........
+                            </div>
+                        </div>
+
+                        <br />
+
+                        <div className='intro-two-container'>
+                            <div className='intro-two'>
+                                :????
+                            </div>
+                            <div className='intro-two'>
+                                World..........
+                            </div>
+                        </div>
+
                         <button onClick={() => this.startGame()} className="intro-next">Next</button>
                     </div>
                 </div>
