27cac45444dcda5959f815f26e0a38611384f5b9
walterlee <walter.lee9@gmail.com>
2019-07-06 18:29:22 -0500

    first floor house images added

--- src/components/assets/firstLocation/firstLocation.jsx
+++ src/components/assets/firstLocation/firstLocation.jsx
@@ -1,8 +1,38 @@
 import frontYard from './front-yard.jpg';
+import hallway from './hallway.jpg';
+import diningRoom from './diningRoom.jpg';
+import kitchen from './kitchen.jpg';
+import basement from './basement.jpg';
+import livingRoom from './livingRoom.jpeg';
+import studyRoom from './studyRoom.jpg';
 
 export const firstLocationImages = [
     {
         id: 1,
         url: frontYard
+    },
+    {
+        id: 2,
+        url: hallway
+    },
+    {
+        id: 3,
+        url: diningRoom
+    },
+    {
+        id: 4,
+        url: kitchen
+    },
+    {
+        id: 5,
+        url: basement
+    },
+    {
+        id: 6,
+        url: livingRoom
+    },
+    {
+        id: 7,
+        url: studyRoom
     }
 ]
