8b80d15ccfd169bee625e4a93f4d35cd99a40e74
walterlee <walter.lee9@gmail.com>
2019-07-06 19:06:41 -0500

    added house second floor images

--- src/components/assets/firstLocation/firstLocation.jsx
+++ src/components/assets/firstLocation/firstLocation.jsx
@@ -5,6 +5,13 @@ import kitchen from './kitchen.jpg';
 import basement from './basement.jpg';
 import livingRoom from './livingRoom.jpeg';
 import studyRoom from './studyRoom.jpg';
+import hallwayTop from './hallwayTop.jpg';
+import masterBedroom from './masterBedroom.jpg';
+import bedroom1 from './bedroom1.gif';
+import bedroom2 from './bedroom2.jpg';
+import bedroom3 from './bedroom3.jpg';
+import attic from './attic.jpg';
+import bossRoom from './bossRoom.jpg';
 
 export const firstLocationImages = [
     {
@@ -34,5 +41,37 @@ export const firstLocationImages = [
     {
         id: 7,
         url: studyRoom
+    },
+    {
+        id: 8,
+        url: hallwayTop
+    },
+    {
+        id: 9,
+        url: masterBedroom
+    },
+    {
+        id: 10,
+        url: bedroom1
+    },
+    {
+        id: 11,
+        url: bedroom2
+    },
+    {
+        id: 12,
+        url: bedroom3
+    },
+    {
+        id: 13,
+        url: masterBedroom
+    },
+    {
+        id: 14,
+        url: attic
+    },
+    {
+        id: 15,
+        url: bossRoom
     }
 ]
