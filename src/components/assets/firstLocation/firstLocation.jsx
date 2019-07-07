047e653a8a68624d746efdf0e0f2b80169d47328
walterlee <walter.lee9@gmail.com>
2019-07-06 19:15:12 -0500

    added floor level values

--- src/components/assets/firstLocation/firstLocation.jsx
+++ src/components/assets/firstLocation/firstLocation.jsx
@@ -16,76 +16,91 @@ import bossRoom from './bossRoom.jpg';
 export const firstLocationImages = [
     {
         id: 1,
+        floor: '1st',
         location: 'Front Yard',
         url: frontYard
     },
     {
         id: 2,
+        floor: '1st',
         location: 'First Hallway',
         url: hallway
     },
     {
         id: 3,
+        floor: '1st',
         location: 'Dining Room',
         url: diningRoom
     },
     {
         id: 4,
+        floor: '1st',
         location: 'Kitchen',
         url: kitchen
     },
     {
         id: 5,
+        floor: 'Sub',
         location: 'Basement',
         url: basement
     },
     {
         id: 6,
+        floor: '1st',
         location: 'Living Room',
         url: livingRoom
     },
     {
         id: 7,
+        floor: '1st',
         location: 'Study Room',
         url: studyRoom
     },
     {
         id: 8,
+        floor: '2nd',
         location: 'Second Hallway',
         url: hallwayTop
     },
     {
         id: 9,
+        floor: '2nd',
         location: 'Master Bedroom',
         url: masterBedroom
     },
     {
         id: 10,
+        floor: '2nd',
         location: 'First Bedroom',
         url: bedroom1
     },
     {
         id: 11,
+        floor: '2nd',
         location: 'Second Bedroom',
         url: bedroom2
     },
     {
         id: 12,
+        floor: '2nd',
         location: 'Third Bedroom',
         url: bedroom3
     },
     {
         id: 13,
+        floor: '2nd',
         location: 'Master Bedroom',
         url: masterBedroom
     },
     {
         id: 14,
+        floor: '3rd',
         location: 'Attic',
         url: attic
     },
     {
         id: 15,
+        floor: '2nd',
         location: 'Room',
         url: bossRoom
     }
