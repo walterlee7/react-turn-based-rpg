1adee80219ea82824b01a0cacde85be4eb39ec38
walterlee <walter.lee9@gmail.com>
2019-07-06 19:11:25 -0500

    added location values

--- src/components/assets/firstLocation/firstLocation.jsx
+++ src/components/assets/firstLocation/firstLocation.jsx
@@ -16,62 +16,77 @@ import bossRoom from './bossRoom.jpg';
 export const firstLocationImages = [
     {
         id: 1,
+        location: 'Front Yard',
         url: frontYard
     },
     {
         id: 2,
+        location: 'First Hallway',
         url: hallway
     },
     {
         id: 3,
+        location: 'Dining Room',
         url: diningRoom
     },
     {
         id: 4,
+        location: 'Kitchen',
         url: kitchen
     },
     {
         id: 5,
+        location: 'Basement',
         url: basement
     },
     {
         id: 6,
+        location: 'Living Room',
         url: livingRoom
     },
     {
         id: 7,
+        location: 'Study Room',
         url: studyRoom
     },
     {
         id: 8,
+        location: 'Second Hallway',
         url: hallwayTop
     },
     {
         id: 9,
+        location: 'Master Bedroom',
         url: masterBedroom
     },
     {
         id: 10,
+        location: 'First Bedroom',
         url: bedroom1
     },
     {
         id: 11,
+        location: 'Second Bedroom',
         url: bedroom2
     },
     {
         id: 12,
+        location: 'Third Bedroom',
         url: bedroom3
     },
     {
         id: 13,
+        location: 'Master Bedroom',
         url: masterBedroom
     },
     {
         id: 14,
+        location: 'Attic',
         url: attic
     },
     {
         id: 15,
+        location: 'Room',
         url: bossRoom
     }
 ]
